43501f1d268adaa68045b3b30418771a3d14274c
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-06-23 18:04:21 +0800

    Add HeroSection component with gradient background

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+
+</script>
+<template>
+    <section class="w-full h-screen text-white flex flex-col justify-center items-center text-center px-4" style="
+             background-image: linear-gradient(
+               to bottom left,
+               #1C232A 0%,
+               #1C232A 6%,
+               #1C232A 13%,
+               #1D242B 19%,
+               #20262D 25%,
+               #252B32 31%,
+               #2F343A 38%,
+               #404348 44%,
+               #5A595D 50%,
+               #736F71 56%,
+               #847E7F 63%,
+               #8E8787 69%,
+               #938C8C 75%,
+               #968E8E 81%,
+               #978F8F 88%,
+               #978F8F 94%,
+               #978F8F 100%
+             );
+           ">
+        <h1 class="text-5xl font-bold mb-4">Tagline</h1>
+        <p class="text-xl mb-6">Context</p>
+    </section>
+</template>
