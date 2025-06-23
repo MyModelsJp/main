f835497a1e2fa1a79caed896f370cf4933acb09b
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-06-23 17:37:33 +0800

    Add vue-router and integrate routing into app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+import Layout from './layout/Layout.vue';
+import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
+    <Layout>
+        <main>
+            <RouterView />
+        </main>
+    </Layout>
 </template>
