96fa6f158b99b5a11b12592f91ce9ae16f486af5
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-06-24 18:05:22 +0800

    Remove Contact page and its route

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,10 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 import Homepage from "./Pages/Homepage.vue";
 import About from "./Pages/About.vue";
-import Contact from "./Pages/Contact.vue";
 
 const routes = [
   { path: "/", component: Homepage, name: "Home" },
   { path: "/about", component: About, name: "About" },
-  { path: "/contact", component: Contact, name: "Contact" },
 ];
 
 export const router = createRouter({
