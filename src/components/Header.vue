d434fd47e58a398ec849278335487a3f574b19b4
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-07-08 20:52:59 +0800

    Remove unused import from Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 import { RouterLink } from 'vue-router';
 import { motion } from "motion-v"
 
