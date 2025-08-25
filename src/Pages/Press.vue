d71ba0432af9c82d193c63ee431dcba8be0ceca8
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-08-25 15:59:38 +0800

    Update Press page layout and improve text formatting

--- src/Pages/Press.vue
+++ src/Pages/Press.vue
@@ -18,17 +18,17 @@ const events = [
     <div
         class="max-w-screen-2xl px-10 mx-auto min-h-dvh text-center flex flex-col items-center justify-center space-y-5">
         <h1 class="text-6xl">press</h1>
-        <p class="justify-center px-14">we cater to a bunch of events,,, insert filler text
+        <p class="justify-center px-14 mb-10">we cater to a bunch of events,,, insert filler text
         </p>
 
         <Section class="w-full h-full flex flex-col space-y-6 gap-8 mb-10">
             <template v-for="(i_, index) in Array(3)">
-                <div class="flex flex-col md:grid md:grid-cols-5 gap-8">
+                <div class="flex flex-col md:grid md:grid-cols-5 gap-8 items-center justify-center">
                     <div class="w-full h-96 col-span-3" :class="index % 2 === 0 ? 'md:order-1' : 'md:order-2'">
                         <img :src="events[0].img" alt="placeholder" class="object-cover h-full w-full">
                     </div>
                     <div class="w-full h-full col-span-2" :class="index % 2 === 0 ? 'md:order-2' : 'md:order-1'">
-                        <p class="text-ellipsis">{{ events[0].description }}</p>
+                        <p class="whitespace-pre-line text-ellipsis">{{ events[0].description }}</p>
                     </div>
                 </div>
             </template>
